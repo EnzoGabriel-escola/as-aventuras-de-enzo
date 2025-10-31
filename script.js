4363ef6a5b5788095c85b23efa9656945d874c65
EnzoGabriel-escola <enzo.souza.mendes@escola.pr.gov.br>
2025-10-31 09:03:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const avanca = document.querySelectorALL('.btn-proximo');
 
+avança.forEach(button => {
+button.addEventListener ('click', function(){
+  const atual = document.querySelector('.ativo);
+  const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+  atual.classList.remove('ativo')
+  document.getElementByid(proximoPasso).classList.add('ativo');
+}
+                         }

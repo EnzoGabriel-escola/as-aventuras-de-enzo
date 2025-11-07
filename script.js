9027abe4d65d00d5c68ceb35bb1804444fb9221a
EnzoGabriel-escola <enzo.souza.mendes@escola.pr.gov.br>
2025-11-07 07:29:43 -0300

    Fix typo in variable name for button selection

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const avanca = document.querySelectorALL('.btn-proximo');
 
-avança.forEach(button => {
+avanca.forEach(button => {
 button.addEventListener ('click', function(){
   const atual = document.querySelector('.ativo);
   const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
